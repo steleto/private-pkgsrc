850a42e356fa7f66d093ade8c1a5d315b9006555
Atsushi Toyokura <asteria.at@gmail.com>
2020-08-03 23:32:05 +0900

    cog-vm: update private/cog-vm to 5.0-202003021730
    * 202003021730
        Release for Squeak 5.3

--- cog-vm/patches/patch-platforms_unix_vm_sqUnixMain.c
+++ cog-vm/patches/patch-platforms_unix_vm_sqUnixMain.c
@@ -1,14 +1,14 @@
 $NetBSD$
 
---- platforms/unix/vm/sqUnixMain.c.orig	2018-10-19 04:12:21.000000000 +0000
+--- platforms/unix/vm/sqUnixMain.c.orig	2020-03-02 17:30:55.000000000 +0000
 +++ platforms/unix/vm/sqUnixMain.c
 @@ -933,6 +933,9 @@ reportStackState(char *msg, char *date, 
  # elif __FreeBSD__ && __amd64__
- 			void *fp = (void *)(uap ? uap->uc_mcontext.mc_rbp: 0);
- 			void *sp = (void *)(uap ? uap->uc_mcontext.mc_rsp: 0);
+ 			void *fp = (void *)(uap ? uap->uc_mcontext.mc_rbp : 0);
+ 			void *sp = (void *)(uap ? uap->uc_mcontext.mc_rsp : 0);
 +# elif __NetBSD__ && __x86_64__
-+			void *fp = (void *)(uap ? uap->uc_mcontext.__gregs[_REG_RBP]: 0);
-+			void *sp = (void *)(uap ? uap->uc_mcontext.__gregs[_REG_RSP]: 0);
++			void *fp = (void *)(uap ? uap->uc_mcontext.__gregs[_REG_RBP] : 0);
++			void *sp = (void *)(uap ? uap->uc_mcontext.__gregs[_REG_RSP] : 0);
  # elif __OpenBSD__ && __i386__
- 			void *fp = (void *)(uap ? uap->sc_ebp: 0);
- 			void *sp = (void *)(uap ? uap->sc_esp: 0);
+ 			void *fp = (void *)(uap ? uap->sc_ebp : 0);
+ 			void *sp = (void *)(uap ? uap->sc_esp : 0);
