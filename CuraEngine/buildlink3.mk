3e400a6519baba4cf6696c15ec6abec9119380d1
Atsushi Toyokura <asteria.at@gmail.com>
2021-01-12 00:30:56 +0900

    CuraEngine: Import CuraEngine-4.8.0 as private/CuraEngine

---
+++ CuraEngine/buildlink3.mk
@@ -0,0 +1,15 @@
+# $NetBSD$
+
+BUILDLINK_TREE+=	CuraEngine
+
+.if !defined(CURAENGINE_BUILDLINK3_MK)
+CURAENGINE_BUILDLINK3_MK:=
+
+BUILDLINK_API_DEPENDS.CuraEngine+=	CuraEngine>=4.8.0
+BUILDLINK_PKGSRCDIR.CuraEngine?=	../../private/CuraEngine
+
+.include "../../devel/protobuf/buildlink3.mk"
+.include "../../private/libArcus/buildlink3.mk"
+.endif	# CURAENGINE_BUILDLINK3_MK
+
+BUILDLINK_TREE+=	-CuraEngine
