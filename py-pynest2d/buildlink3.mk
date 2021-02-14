36a1774cd0fc3fdeeb3ee2229184f8268e444876
Atsushi Toyokura <asteria.at@gmail.com>
2021-02-14 17:44:51 +0900

    py-pynest2d: Use c++14

--- py-pynest2d/buildlink3.mk
+++ py-pynest2d/buildlink3.mk
@@ -1,15 +1,15 @@
 # $NetBSD$
 
-BUILDLINK_TREE+=	py37-pynest2d
+BUILDLINK_TREE+=	py-pynest2d
 
-.if !defined(PY37_PYNEST2D_BUILDLINK3_MK)
-PY37_PYNEST2D_BUILDLINK3_MK:=
+.if !defined(PY_PYNEST2D_BUILDLINK3_MK)
+PY_PYNEST2D_BUILDLINK3_MK:=
 
-BUILDLINK_API_DEPENDS.py37-pynest2d+=	py37-pynest2d>=4.8.0
-BUILDLINK_PKGSRCDIR.py37-pynest2d?=	../../private/py-pynest2d
+BUILDLINK_API_DEPENDS.py-pynest2d+=	${PYPKGPREFIX}-pynest2d>=4.8.0
+BUILDLINK_PKGSRCDIR.py-pynest2d?=	../../private/py-pynest2d
 
 .include "../../private/libnest2d-ultimaker/buildlink3.mk"
 .include "../../x11/py-sip/buildlink3.mk"
-.endif	# PY37_PYNEST2D_BUILDLINK3_MK
+.endif	# PY_PYNEST2D_BUILDLINK3_MK
 
-BUILDLINK_TREE+=	-py37-pynest2d
+BUILDLINK_TREE+=	-py-pynest2d
