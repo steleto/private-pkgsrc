680d23af516b2b96c22c2cd5f1b90689f99ca393
Atsushi Toyokura <asteria.at@gmail.com>
2021-02-24 00:17:22 +0900

    pharo-vm: update 5.0-202003021730 (work in progress)

--- pharo-vm/patches/patch-spur64src_vm_gcc3x-cointerp.c
+++ pharo-vm/patches/patch-spur64src_vm_gcc3x-cointerp.c
@@ -1,5 +1,18 @@
 $NetBSD$
 
+--- spur64src/vm/cogitX64SysV.c.orig	2021-02-18 16:42:20.999184653 +0000
++++ spur64src/vm/cogitX64SysV.c
+@@ -22288,9 +22288,7 @@ dispatchConcretize(AbstractInstruction *
+ 			((self_in_dispatchConcretize->operands))[2] = 0;
+ 			return savedSize1;
+ 		}
+-		if ((reg20 == RAX)
+-		 || ((reg20 == RBP)
+-		 || (reg20 == RSP))) {
++		if (reg20 == RAX) {
+ 			offset38 = 0;
+ 		}
+ 		else {
 --- spur64src/vm/gcc3x-cointerp.c.orig	2018-11-08 15:09:10.122448376 +0000
 +++ spur64src/vm/gcc3x-cointerp.c
 @@ -41082,8 +41082,8 @@ fetchClassTagOf(sqInt oop)
