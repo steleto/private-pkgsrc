eb3e0f9f6c7d8726620c3d43d52df70f86a1e5de
Atsushi Toyokura <asteria.at@gmail.com>
2021-08-22 17:15:25 +0900

    fdm_materials: Update private/fdm_materials to 4.10.0

--- fdm_materials/buildlink3.mk
+++ fdm_materials/buildlink3.mk
@@ -5,7 +5,7 @@ BUILDLINK_TREE+=	fdm_materials
 .if !defined(FDM_MATERIALS_BUILDLINK3_MK)
 FDM_MATERIALS_BUILDLINK3_MK:=
 
-BUILDLINK_API_DEPENDS.fdm_materials+=	fdm_materials>=4.8.0
+BUILDLINK_API_DEPENDS.fdm_materials+=	fdm_materials>=4.10.0
 BUILDLINK_PKGSRCDIR.fdm_materials?=	../../private/fdm_materials
 .endif	# FDM_MATERIALS_BUILDLINK3_MK
 
